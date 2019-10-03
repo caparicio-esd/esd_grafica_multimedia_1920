3e29131d9d4a6c506c6c3175bc47f8ef2c5c2019
apabook <apabook@MacBook-Pro-de-apabook.local>
2019-10-03 11:52:04 +0200

    subir ejercicio

---
+++ 20191003_01--password.js
@@ -0,0 +1,59 @@
+/**
+ * USER Y PASSWORD CHECKER
+ * Metemos un user_name y una password y tenemos que hacer las siguientes comprobaciones:
+ */
+
+let user = prompt('Mete tu nombre de usuario');
+let password = prompt('Mete tu contraseña');
+
+console.log(`
+    user: ${user}
+    pass: ${password}
+`);
+
+/**
+ * Que el user_name esté en minúsculas
+ */
+
+/**
+ * Que el user_name no tenga espacios
+ */
+
+/**
+ * Que el user_name no tenga ninguno de los siguientes símbolos: & ! $
+ */
+
+/**
+ * Que el user_name no tenga ninguno de estos símbolos á é í ó ú ñ
+ */
+
+/**
+ * Que la constraseña tenga más de 6 caracteres y menos de 14
+ */
+
+/**
+ * Que la contraseña tenga alguna mayúscula
+ */
+
+/**
+ * Que la contraseña tenga alguna minúscula
+ */
+
+/**
+ * Que la contraseña contenga alguna arroba
+ */
+
+/**
+ * Que la contraseña contenga alguno de los siguentes símbolos: & ! $ _
+ */
+
+/**
+ * Que la contraseña NO contenga sólo números
+ */
+
+/**
+ * Que la contraseña contenga algún número
+ */
+
+
+
