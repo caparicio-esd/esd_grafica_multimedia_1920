3070d55a48558b409b71a7d6e3c628b590800b08
apabook <apabook@172-15-68-20.lightspeed.austtx.sbcglobal.net>
2019-10-10 18:40:38 +0200

    subir ejercicio

---
+++ 20191010_01--ejercicioFechas.js
@@ -0,0 +1,24 @@
+// Hora actual
+
+// 3 horas después
+
+// 5 horas antes
+
+// hace 1 semana
+
+// let semanaEnTiempo = 1000 * 60 * 60 * 24 * 7;
+// let haceUnaSemanaTimeStamp = now.getTime() - semanaEnTiempo;
+// let haceUnaSemana = new Date(haceUnaSemanaTimeStamp);
+// console.log(haceUnaSemana);
+
+// dentro de 10 años
+
+// Día de vuestro nacimiento
+
+// 100 años después de vuestro nacimineto
+
+// 30 años antes de vuestro nacimiento
+
+// 2500 años antes de vuestro nacimiento
+
+
---
+++ 20191010_02--relojito.js
@@ -0,0 +1,13 @@
+
+window.setInterval(() => {
+
+    now = new Date();
+    let hour = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let msg = `Son las ${hour}:${minutes}:${seconds}`;
+
+    document.querySelector('h1').innerHTML = msg;
+
+}, 1000);
+
