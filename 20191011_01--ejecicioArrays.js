4de3e6777ac24672babfcdef9f591523b5dbfec3
apabook <apabook@172-15-68-20.lightspeed.austtx.sbcglobal.net>
2019-10-11 16:45:30 +0200

    subir ejercicio

---
+++ 20191011_01--ejecicioArrays.js
@@ -0,0 +1,28 @@
+let pacientesEspera = [
+    "Joaquín",
+    "Miguel",
+    "Ana María",
+    "Paco",
+    "Irene",
+    "Inés",
+    "Zx7781",
+    "Ambrosio"
+];
+
+let pacientesAtendidos = [
+
+];
+
+
+
+// Meter un par de pacientes nuevos al final de la cola que son Manolo y Paula
+
+// Juan Carlos I y se pone el primero
+
+// Borrar Juan Carlos I
+
+// Borrar a Irene
+
+// Borrar Inés y en su lugar metemos a Juan
+
+// Ambrosio lo curamos y lo atendemos
